ec4561b13a97716946c464576047f914afcc0341
Gregdev <iheanemegregory9@gmail.com>
2023-02-16 22:41:19 +0100

    updated size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 /**
- * main -  Entry point
+ * main -  Print size of types
  *
  * Description: 'A program that prints the sizes of data types'
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
     printf("Size of char: %zu(s)\n", sizeof(char));
+
     printf("Size of int: %zu(s)\n", sizeof(int));
+
     printf("Size of long long int: %zu(s)\n", sizeof(long long int));
+
     printf("Size of long int: %zu(s)\n", sizeof(long int));
+
     printf("Size of float: %zu(s)\n", sizeof(float));
     return (0);
 }
