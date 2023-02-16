6cb71cdc43ca2db75d271139515724b907813982
Gregdev <iheanemegregory9@gmail.com>
2023-02-16 22:36:04 +0100

    updated size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 int main(void)
 
 {
-    printf("Size of char: %lu(s)", sizeof(char));
-    printf("Size of int: %lu(s)", sizeof(int));
-    printf("Size of long long int: %lu(s)", sizeof(long long int));
-    printf("Size of long int: %lu(s)", sizeof(long int));
-    printf("Size of float: %lu(s)", sizeof(float));
+    printf("Size of char: %zu(s)", sizeof(char));
+    printf("Size of int: %zu(s)", sizeof(int));
+    printf("Size of long long int: %zu(s)", sizeof(long long int));
+    printf("Size of long int: %zu(s)", sizeof(long int));
+    printf("Size of float: %zu(s)", sizeof(float));
     return (0);
 }
