b13c9cfde4d5ba8bea6503dbedcba0fb3dbca697
Gregory <iheanemegregory9@gmail.com>
2023-02-21 12:33:06 -0800

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints out _putchar
+ *
+ * Description: print _putchar
+ *
+ *Return (0)
+ */
+
+int main(void)
+{
+	char[] = "_putchar";
+	int g = 0;
+
+	for (g = 0; g < 8; g++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar("\n");
+
+	return (0);
+}
