c2ed29225156f222299ca7bde2c2b65a8a1abe1c
Gregory <iheanemegregory9@gmail.com>
2023-02-22 13:40:01 -0800

    added add file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add- adds two ints
+ *
+ * @nO - first int
+ * @n1- second int
+ * Description: Adds two ints
+ *
+ * Return: sum of the added ints
+ */
+
+int add(int n0, int n1)
+{
+	int result;
+
+	result = n0 + n1;
+	return (result);
+}
