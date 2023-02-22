14abe036d6adb7c3fe839b20e8da566b0bf29aec
Gregory <iheanemegregory9@gmail.com>
2023-02-22 10:45:36 -0800

    added abs file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * int _abs - prints the absolute value of an integrer
+ *
+ * @n: n is the integer
+ *
+ * Return: n(the integer)
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
