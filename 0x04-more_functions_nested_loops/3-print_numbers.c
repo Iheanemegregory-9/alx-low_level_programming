26839954901f8846f52d9d5cb546ee9598d2e2cb
Gregory <iheanemegregory9@gmail.com>
2023-02-24 05:06:45 -0800

    added print numbers file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers- prints numbers from 0-9
+ *
+ * Return: Void
+ */
+
+int print_numbers(void)
+{
+	int g;
+	
+	for (g = '0'; g <= '9'; g++)
+	{
+		_putchar(c);
+	}
+	_putchar("\n");
+}
