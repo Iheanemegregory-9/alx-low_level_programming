f5e92d840a408c70928d9c8e6040f130f16d5d54
Gregory <iheanemegregory9@gmail.com>
2023-02-24 03:09:04 -0800

    added more functions nested loops folder

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper- looks for uppercase chars.
+ * @c - the char used to check
+ * Return: returns 1 if c == uppercase and return 0 if c != uppercase
+ */
+
+int _isupper(int c)
+{
+	return (c >= "A" && c <= "Z");
+}
