234cf066422be5573dcd072c2c67c02e4672c1ff
Gregory <iheanemegregory9@gmail.com>
2023-02-27 09:52:04 -0800

    added pointers arrays strings dir.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update value of varaible
+ *
+ * points to 98
+ *
+ * @g: pointer updateed by the variable
+ */
+
+int reset_to_98(int *g)
+{
+	*g = 98
+}
